e4478f8e0d5a41eb301d3c252445bbd4f67a4b79
hagenek <georghagen@gmail.com>
2021-02-12 17:12:13 +0100

    Cleaned up the page a bit

--- blazor-battles/Client/Program.cs
+++ blazor-battles/Client/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;   
 
 namespace blazor_battles.Client
 {
