0658f73615298b90fdbd082eca3c61003fa0b662
hagenek <georghagen@gmail.com>
2021-02-15 23:44:13 +0100

    created an API endpoint that serves data about the different types of units that are available. Served up in JSON format.

--- blazor-battles/Client/Services/IUnitService.cs
+++ blazor-battles/Client/Services/IUnitService.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using blazor_battles.Shared;
 
 namespace blazor_battles.Client.Services
 {
     public interface IUnitService
     {
-        IList<Unit> Units { get; }
+        IList<Unit> Units { get; set; }
         IList<UserUnit> MyUnits { get; set; }
         void AddUnit(int unitId);
+
+        Task LoadUnitsAsync();
     }
 }
--- blazor-battles/Client/Services/UnitService.cs
+++ blazor-battles/Client/Services/UnitService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using blazor_battles.Shared;
 using Blazored.Toast.Services;
 
@@ -10,18 +13,15 @@ namespace blazor_battles.Client.Services
     public class UnitService : IUnitService
     {
         private readonly IToastService _toastService;
+        private readonly HttpClient _http;
 
-        public UnitService(IToastService toastService)
+        public UnitService(IToastService toastService, HttpClient http)
         {
             _toastService = toastService;
+            _http = http;
         }
-        public IList<Unit> Units { get; } = new List<Unit>
-        {
-            new Unit {Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100},
-            new Unit {Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150},
-            new Unit {Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200},
-        };
 
+        public IList<Unit> Units { get; set; } = new List<Unit>();
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
 
         public void AddUnit(int unitId)
@@ -29,6 +29,14 @@ namespace blazor_battles.Client.Services
             var unit = Units.First(unit1 => unit1.Id == unitId);
             MyUnits.Add(new UserUnit {UnitId = unit.Id, HitPoints = unit.HitPoints});
             _toastService.ShowSuccess($"Your {unit.Title} has been built!", "Unit built");
+        }
+
+        public async Task LoadUnitsAsync()
+        {
+            if (Units.Count == 0)
+            {
+                Units = await _http.GetFromJsonAsync<IList<Unit>>("api/unit");
+            }
         }
     }
 }
