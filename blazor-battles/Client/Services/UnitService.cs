48da4e3062c098b2cc696e5253c3c003382aff25
hagenek <georghagen@gmail.com>
2021-02-15 19:03:05 +0100

    Made a registration page with validation

--- blazor-battles/Client/Services/UnitService.cs
+++ blazor-battles/Client/Services/UnitService.cs
@@ -19,14 +19,14 @@ namespace blazor_battles.Client.Services
         {
             new Unit {Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100},
             new Unit {Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150},
-            new Unit {Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200}
+            new Unit {Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200},
         };
 
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
 
         public void AddUnit(int unitId)
         {
-            Unit unit = Units.First(unit1 => unit1.Id == unitId);
+            var unit = Units.First(unit1 => unit1.Id == unitId);
             MyUnits.Add(new UserUnit {UnitId = unit.Id, HitPoints = unit.HitPoints});
             _toastService.ShowSuccess($"Your {unit.Title} has been built!", "Unit built");
         }
