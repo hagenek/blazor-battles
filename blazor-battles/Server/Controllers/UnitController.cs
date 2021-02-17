16e975d64a266ed5c1735f5d094e412e2b5a2790
hagenek <georghagen@gmail.com>
2021-02-17 11:02:45 +0100

    created CRUD routing for unit db

--- blazor-battles/Server/Controllers/UnitController.cs
+++ blazor-battles/Server/Controllers/UnitController.cs
@@ -10,23 +10,65 @@ using Microsoft.EntityFrameworkCore;
 
 namespace blazor_battles.Server
 {
-[Route("api/[controller]")]
-[ApiController]
-public class UnitController : ControllerBase
-{
-    private readonly DataContext _context;
-
-    public UnitController(DataContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
     {
-        _context = context;
-    }
+        private readonly DataContext _context;
 
-    [HttpGet]
-    public async Task <IActionResult> GetUnits()
-    {
-        var units = await _context.Units.ToListAsync();
-        return Ok(units);
-    }
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetUnits()
+        {
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
+        }
+
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            await _context.Units.AddAsync(unit);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Units.ToListAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given Id not found.");
+            }
+
+            dbUnit.BananaCost = unit.BananaCost;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.HitPoints = unit.HitPoints;
+            dbUnit.Title = unit.Title;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given Id not found.");
+            }
+
+            _context.Remove(dbUnit);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+
+        }
     }
 }
