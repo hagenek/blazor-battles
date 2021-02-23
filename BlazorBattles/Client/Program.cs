1bfbfc71b726c171e2c66a2a6316a1797c7015f9
hagenek <georghagen@gmail.com>
2021-02-23 10:55:23 +0100

    Added rest endpoint for getting leaderboard data, and a service go fetch it into the frontend

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -30,6 +30,7 @@ namespace BlazorBattles.Client
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorBattles/Client/Services/ILeaderboardService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorBattles.Shared;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface ILeaderboardService
+    {
+        IList<UserStatistic> Leaderboard { get; set; }
+        Task GetLeaderboard();  
+    }
+}
